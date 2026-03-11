b4666b15621ed512edf82feae8f100d759ccaeb2
irolram <ivanr9016@gmail.com>
2026-03-11 11:46:49 +0100

    Acabado

--- app/src/main/java/com/example/proyectomongodb/service/RetrofitObject.kt
+++ app/src/main/java/com/example/proyectomongodb/service/RetrofitObject.kt
@@ -9,9 +9,9 @@ object RetrofitObject {
     // Instancia de Retrofit
     val api: ApiBibliotecaService by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+           .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+           .build()
             .create(ApiBibliotecaService::class.java)
-    }
+   }
 }
